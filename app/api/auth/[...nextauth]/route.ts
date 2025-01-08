70ba0a8b81fd8b4738c9a304f0eac23bd6e4b5b4
dev-shahed <shahedthedev@gmail.com>
2025-01-09 00:00:47 +0600

    issue fixed with build app

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,12 @@
 import { handlers } from "../../../../auth";
 
-export const { GET, POST } = handlers;
+export const GET =
+  handlers?.GET ||
+  (() => {
+    throw new Error("GET handler is not defined");
+  });
+export const POST =
+  handlers?.POST ||
+  (() => {
+    throw new Error("POST handler is not defined");
+  });
